154ad7a24dabd21f2dfd1e57f70a5741c0443524
Joseph Song <song.josephmin@gmail.com>
2023-04-17 14:34:15 -0700

    Initial Commit

--- Javascript_Unit/Likes/script.js
+++ Javascript_Unit/Likes/script.js
@@ -1,13 +1,3 @@
-var neilLikes = document.querySelector("#neil-likes");
-var nicholeLikes = document.querySelector("#nichole-likes");
-var jimLikes = document.querySelector("#jim-likes");
-
-
-var neilLikesCounter = 9;
-var nicholeLikesCounter = 12;
-var jimLikesCounter = 9;
-
-
 function increaseLikes(id) {
     var spanTag = document.querySelector('#' + id);
     var num = parseInt(spanTag.innerText);
