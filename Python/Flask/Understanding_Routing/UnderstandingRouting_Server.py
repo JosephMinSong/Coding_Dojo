aff79c02b9956c4ec8d3be3e3fdf1043c4da0b68
Joseph Song <song.josephmin@gmail.com>
2023-05-03 14:48:14 -0700

    Initial Commit

---
+++ Python/Flask/Understanding_Routing/UnderstandingRouting_Server.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('/')
+def welcome_message():
+    return "Hello World!"
+
+@app.route('/dojo')
+def dojo_message():
+    return "Dojo!"
+
+@app.route('/say/<string:name>')
+def say_my_name(name):
+    return f"Hi {name}!"
+
+@app.route('/repeat/<int:num>/<string:string>')
+def repeat_my_str(num, string):
+    return (string + " ") * num
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return "Sorry! No response. Try again.", 404
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
