2e681d423525104cf5838e43ab6e6325e4132b9c
Joseph Song <song.josephmin@gmail.com>
2023-05-06 11:32:59 -0700

    Initial Commit

--- Python/Flask/DojoSurvey/server.py
+++ Python/Flask/DojoSurvey/server.py
@@ -1,17 +1,30 @@
 from flask import Flask, render_template, session, redirect, request
 app = Flask(__name__)
+app.secret_key = 'secretkey'
+
+# Render Templates
 @app.route('/')
 def index():
     return render_template("index.html")
 
+@app.route('/result')
+def result():
+    return render_template("result.html")
 
+
+
+# POST Methods
 @app.route('/process', methods=['POST'])
 def process():
     session['username'] = request.form['username']
-    
+    session['location'] = request.form['location']
+    session['comments'] = request.form['comments']
+    session['language'] = request.form.getlist('language')
+
+    return redirect('/result')
 
 
 
-
+# Debug
 if __name__ == '__main__':
     app.run(debug=True)
