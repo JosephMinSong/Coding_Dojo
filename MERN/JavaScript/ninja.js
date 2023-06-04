cb5cb8e09bb6f9058e1595e2f2b6f67e874e9203
Joseph Song <song.josephmin@gmail.com>
2023-06-04 14:40:16 -0700

    added sensei class

--- MERN/JavaScript/ninja.js
+++ MERN/JavaScript/ninja.js
@@ -22,3 +22,19 @@ class Ninja {
         return this
     }
 }
+
+class Sensei extends Ninja {
+    constructor (name) {
+        super (name)
+        this.health = 200
+        this.speed = 10
+        this.strength = 10
+        this.wisdom = 10
+    }
+
+    speakWisdom() {
+        super.drinkSake()
+        console.log("A wise message")
+        return this
+    }
+}
