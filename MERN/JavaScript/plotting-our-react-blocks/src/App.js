97f644c52323188c797b4b6ddb18ff4af63e167b
Joseph Song <song.josephmin@gmail.com>
2023-06-06 20:47:11 -0700

    initial commit

--- MERN/JavaScript/plotting-our-react-blocks/src/App.js
+++ MERN/JavaScript/plotting-our-react-blocks/src/App.js
@@ -1,8 +1,8 @@
-import './App.css';
+import styles from "./App.module.css"
 
 function App() {
   return (
-    <div className="app">
+    <div className = {styles.app}>
     <Header />
     <Navigation />
     <Main>
