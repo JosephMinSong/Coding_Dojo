0253518d97d585df33a10ed7ab61e441e5796e91
Joseph Song <song.josephmin@gmail.com>
2023-06-06 20:38:00 -0700

    initial commit

---
+++ MERN/JavaScript/plotting-our-react-blocks/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+    <Header />
+    <Navigation />
+    <Main>
+        <SubContents />
+        <SubContents />
+        <SubContents />
+        <Advertisement />
+    </Main>
+</div>
+  )
+}
+
+export default App;
