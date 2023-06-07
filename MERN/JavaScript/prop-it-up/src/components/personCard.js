3026f81c6617622be5cec2b13006b5ff7704e7b7
Joseph Song <song.josephmin@gmail.com>
2023-06-06 20:16:02 -0700

    initial commit

--- MERN/JavaScript/prop-it-up/src/components/personCard.js
+++ MERN/JavaScript/prop-it-up/src/components/personCard.js
@@ -1,11 +1,21 @@
+import { useState } from 'react'
+
 
 function PersonCard (props) {
     const { firstname, lastname, age, hairColor } = props
+    
+    const [personAge, setPersonAge] = useState(age)
+
+    const handleClick = () => {
+        setPersonAge(personAge + 1)
+    }
+
     return (
         <div style = {{backgroundColor : "blanchedalmond"}}>
             <h1> {lastname}, {firstname} </h1>
-            <p>Age: {age} </p>
+            <p>Age: {personAge} </p>
             <p>Hair Color: {hairColor} </p>
+            <button onClick = { handleClick }> Birthday Button for {firstname} {lastname} </button>
         </div>
     )
 }
