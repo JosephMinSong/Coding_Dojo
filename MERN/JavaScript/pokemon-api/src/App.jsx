92d6d898e3d6bcaab7bb7bc076fb1f9faf029f04
Joseph Song <song.josephmin@gmail.com>
2023-06-12 19:40:51 -0700

    final commit

--- MERN/JavaScript/pokemon-api/src/App.jsx
+++ MERN/JavaScript/pokemon-api/src/App.jsx
@@ -1,9 +1,28 @@
 import styles from "./App.module.css"
+import { useState } from 'react'
 
 export default function App() {
+    
+    const [pokemons, setPokemons] = useState([])
+
+    const getAllPokemon = () => {
+        fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
+        .then(r => r.json())
+        .then(data => {
+            for (let i = 0; i < data.results.length; i++){
+                setPokemons(p => [...p, { 'id' : i+1 , 'name': data.results[i].name}])
+            }
+        })
+    }
+
     return (
         <div className={ styles.App }>
-            
+            <button onClick={ getAllPokemon }> Get All Pokemon </button>
+            { pokemons.map(pokemon => {
+                return (
+                    <p> {pokemon.id} { pokemon.name } </p>
+                )
+            }) }
         </div>
     )
 }
