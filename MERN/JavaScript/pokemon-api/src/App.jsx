fb30885af8d6d74d189cb8a311abef98b6d79838
Joseph Song <song.josephmin@gmail.com>
2023-06-12 20:01:05 -0700

    added axios and axios method

--- MERN/JavaScript/pokemon-api/src/App.jsx
+++ MERN/JavaScript/pokemon-api/src/App.jsx
@@ -1,16 +1,27 @@
 import styles from "./App.module.css"
 import { useState } from 'react'
+import axios from 'axios'
 
 export default function App() {
     
     const [pokemons, setPokemons] = useState([])
 
+    // Fetch method
+    // const getAllPokemon = () => {
+    //     fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
+    //     .then(r => r.json())
+    //     .then(data => {
+    //         for (let i = 0; i < data.results.length; i++){
+    //             setPokemons(p => [...p, { 'id' : i+1 , 'name': data.results[i].name}])
+    //         }
+    //     })
+    // }
+
+    // Axios method
     const getAllPokemon = () => {
-        fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
-        .then(r => r.json())
-        .then(data => {
-            for (let i = 0; i < data.results.length; i++){
-                setPokemons(p => [...p, { 'id' : i+1 , 'name': data.results[i].name}])
+        axios.get("https://pokeapi.co/api/v2/pokemon?limit=807").then(response => {
+            for (let i = 0; i < response.data.results.length; i++){
+                setPokemons(pokemon => [...pokemon, { 'id' : i+1, 'name' : response.data.results[i].name }])
             }
         })
     }
