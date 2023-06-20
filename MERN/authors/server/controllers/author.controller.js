6de347c513a9aa5c014d4c7a4581c428b61e0d38
Joseph Song <song.josephmin@gmail.com>
2023-06-19 19:47:12 -0700

    added routes

--- MERN/authors/server/controllers/author.controller.js
+++ MERN/authors/server/controllers/author.controller.js
@@ -1,9 +1,34 @@
 const Author = require('../models/author.model')
 
 module.exports = {
+
     getAll : (req,res) => {
         Author.find()
             .then(authors => res.json(authors))
             .catch(err => res.json(err))
+    },
+
+    getOne : (req,res) => {
+        Author.findOne({ _id : req.params.id })
+            .then(author => res.json(author))
+            .catch(err => res.json(err))
+    },
+
+    createOne : (req,res) => {
+        Author.create(req.body)
+            .then(author => res.json(author))
+            .catch(err => res.status(400).json(err))
+    },
+
+    updateOne : (req,res) => {
+        Author.findOneAndUpdate({ _id : req.params.id }, req.body, { runValidators : true, new : true })
+            .then(author => res.json(author))
+            .catch(err => res.status(400).json(err))
+    },
+
+    deleteOne : (req, res) => {
+        Author.findByIdAndDelete({ _id : req.params.id })
+            .then(author => res.json(author))
+            .then(err => res.json(err))
     }
 }
--- MERN/authors/server/routes/author.route.js
+++ MERN/authors/server/routes/author.route.js
@@ -1,5 +1,8 @@
 const AuthorController = require('../controllers/author.controller')
 
 module.exports = app => {
+
+    app.get('/api/test', AuthorController.test)
+
     app.get('/api/authors', AuthorController.getAll)
 }
