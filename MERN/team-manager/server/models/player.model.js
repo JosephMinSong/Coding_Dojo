872cdc07de7f7f076f7aa5f537da3ddff612ed5c
Joseph Song <song.josephmin@gmail.com>
2023-06-20 18:17:25 -0700

    initial commit

--- MERN/team-manager/server/models/player.model.js
+++ MERN/team-manager/server/models/player.model.js
@@ -5,6 +5,25 @@ const PlayerSchema = new mongoose.Schema({
         type : String,
         required : [true, 'Please enter a name'],
         minlength : [2, 'Author name must be at least 2 characters long']
+    },
+
+    prefPosition : {
+        type: String
+    },
+
+    game1 : {
+        type: Number,
+        default : 3
+    },
+
+    game2: {
+        type: Number,
+        default : 3
+    },
+
+    game3: {
+        type: Number,
+        default: 3
     }
 }, { timestamps : true })
 
