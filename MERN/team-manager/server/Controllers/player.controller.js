b3c9a30bb3a7163957b2d02d0eab1034deeabed1
Joseph Min Song <129890601+JosephMinSong@users.noreply.github.com>
2023-06-20 17:38:18 -0700

    Delete MERN/team-manager/server directory

--- MERN/team-manager/server/Controllers/player.controller.js
+++
@@ -1,9 +0,0 @@
-const Player = require('../Models/player.model')
-
-module.exports = {
-    getAll : (req, res) => {
-        Player.find()
-            .then(players => res.json(players))
-            .catch(err => res.json(err))
-    }
-}
--- MERN/team-manager/server/Routes/player.route.js
+++
@@ -1,5 +0,0 @@
-const PlayerController = require('.../controllers/player.controller')
-
-module.exports = app => {
-    app.get('/api/players', PlayerController.getAll)
-}
