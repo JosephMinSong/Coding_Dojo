98b14a07c31e07d5452ba2cb46148a3c79414af6
Joseph Song <song.josephmin@gmail.com>
2023-07-03 19:24:02 -0700

    initial commit

---
+++ C-Sharp/FirstCSharp/Program.cs
@@ -0,0 +1,11 @@
+﻿string name = "Joseph";
+
+int number = 27;
+
+bool IsJoseph = true;
+
+double myOtherNumber = 27.2727;
+
+char myLetter = 'j';
+
+Console.WriteLine($"This is my string: {name}. This is my integer: {number}. This is my boolean: {IsJoseph}. This is my double: {myOtherNumber}. This is my letter: {myLetter}.");
