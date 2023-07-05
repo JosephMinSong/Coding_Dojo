852fdd29dac9abdd2006c904187597de337c511c
Joseph Song <song.josephmin@gmail.com>
2023-07-05 16:06:51 -0700

    Added wine components

--- C-Sharp/OOP/DrinkMaker/Wine.cs
+++ C-Sharp/OOP/DrinkMaker/Wine.cs
@@ -1,16 +1,30 @@
 public class Wine : Drink {
     int AlcoholContent;
+
+    string Region;
+
+    int Year;
     public int _AlcoholContent{
         get { return AlcoholContent; }
     }
 
-    public Wine(string name, string color, double temp, bool isCarb, int calories, int alcoholContent) : base (name, color, temp, isCarb, calories){
+    public string _Region{
+        get { return Region; }
+    }
+
+    public int _Year{
+        get { return Year; }
+    }
+
+    public Wine(string name, string color, double temp, bool isCarb, int calories, int alcoholContent, string region, int year) : base (name, color, temp, isCarb, calories){
         AlcoholContent = alcoholContent;
+        Region = region;
+        Year = year;
     }
 
     public override void ShowInfo()
     {
         base.ShowInfo();
-        Console.WriteLine($"Alcohol Content: {AlcoholContent}");
+        Console.WriteLine($"Alcohol Content: {AlcoholContent}. Region: {Region}. Year: {Year}");
     }
 }
