6b550d260c8336dfe7123990a648e36c78a77bad
Joseph Song <song.josephmin@gmail.com>
2023-07-05 20:15:13 -0700

    added melee and ranged fighter

--- C-Sharp/OOP/GameDeveloper-1/Program.cs
+++ C-Sharp/OOP/GameDeveloper-1/Program.cs
@@ -3,8 +3,22 @@ Attack FireBall = new Attack("FireBall", 20);
 Attack Punch = new Attack("Punch", 5);
 Attack Throw = new Attack("Throw", 10);
 
-One.AddAttack(FireBall).AddAttack(Punch).AddAttack(Throw);
+// One.AddAttack(FireBall).AddAttack(Punch).AddAttack(Throw);
 
-One.ShowInfo();
+// One.ShowInfo();
 
-One.RandomAttack();
+// FROM GAME DEVELOPER 1********************
+
+// One.RandomAttack();
+
+// FROM GAME DEVELOPER 2****************
+
+// MeleeFighter Four = new MeleeFighter("Four");
+// Four.ShowInfo();
+// Four.Rage(One);
+
+RangedFighter Five = new RangedFighter("Five");
+
+Five.ShowInfo();
+Five.Dash();
+Five.PerformAttack(One, ArrowShot);
