4319a134e713f76fd5ba1ad6cfb0d9826d512991
Joseph Song <song.josephmin@gmail.com>
2023-07-06 12:43:09 -0700

    added style

--- C-Sharp/ASPNET_Core/RazorFun/Controllers/RazorfunController.cs
+++ C-Sharp/ASPNET_Core/RazorFun/Controllers/RazorfunController.cs
@@ -2,10 +2,15 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Razorfun.Controllers;
 
-public class RazorfunController : Controller {
+public class RazorfunController : Controller 
+{
 
     [HttpGet("")]
-    public ViewResult Index(){
+    public ViewResult Index()
+    {
+
+        ViewBag.WordList = new List<string>() {"Apple Pie", "Pizza", "Cinnamon Rolls", "Lasagna", "Donuts", "Chocolate Cake", "Burritos"};
+
         return View();
     }
 
