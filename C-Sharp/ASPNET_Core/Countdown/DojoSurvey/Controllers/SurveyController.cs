cf5dcc296e1ccdf990a7ee4e6580f3b144396141
Joseph Song <song.josephmin@gmail.com>
2023-07-06 22:24:06 -0700

    final commit

--- C-Sharp/ASPNET_Core/Countdown/DojoSurvey/Controllers/SurveyController.cs
+++ C-Sharp/ASPNET_Core/Countdown/DojoSurvey/Controllers/SurveyController.cs
@@ -1,12 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace Countdown.Controllers;
+namespace Survey.Controllers;
 
-public class CountdownController : Controller 
+public class SurveyController : Controller 
 {
     [HttpGet("")]
     public ViewResult Index()
     {
+        return View();
+    }
+
+    [HttpPost("processForm")]
+    public IActionResult ProcessForm(string name, string location, string language, string comments)
+    {
+
+        Console.WriteLine($"{name}, {location}, {language}, {comments}");
+
+        return RedirectToAction("Result", new {name=name, location=location, language=language, comments=comments});
+    }
+
+    [HttpGet("results")]
+    public ViewResult Result(string name, string location, string language, string comments)
+    {
+        ViewBag.SurveyResults = new List<string>() {name, location, language, comments};
+
         return View();
     }
 }
