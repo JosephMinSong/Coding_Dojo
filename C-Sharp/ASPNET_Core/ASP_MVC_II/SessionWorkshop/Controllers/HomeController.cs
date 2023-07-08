5072228d4316edee2bb56bd095062ac3602e6b2a
Joseph Song <song.josephmin@gmail.com>
2023-07-08 16:28:28 -0700

    final commit

--- C-Sharp/ASPNET_Core/ASP_MVC_II/SessionWorkshop/Controllers/HomeController.cs
+++ C-Sharp/ASPNET_Core/ASP_MVC_II/SessionWorkshop/Controllers/HomeController.cs
@@ -15,7 +15,8 @@ public class HomeController : Controller
 
     [HttpGet("")]
     public IActionResult Index()
-    {
+    {   
+        HttpContext.Session.SetInt32("number", 22);
         return View();
     }
 
@@ -36,8 +37,47 @@ public class HomeController : Controller
     [HttpGet("dashboard")]
     public IActionResult Dashboard()
     {
+        if (HttpContext.Session.GetString("sessionUser") == null)
+        {
+            return RedirectToAction("Index");
+        }
         ViewBag.sessionUsername = HttpContext.Session.GetString("sessionUser");
         return View("Dashboard");
+    }
+
+    [HttpPost("changeNumber")]
+    public IActionResult ChangeNumber(string operation)
+    {
+        int? sessionNumber = HttpContext.Session.GetInt32("number");
+        if(operation == "add")
+        {
+            sessionNumber += 1;
+            HttpContext.Session.SetInt32("number", (int)sessionNumber);
+        }
+        if(operation == "minus")
+        {
+            sessionNumber -= 1;
+            HttpContext.Session.SetInt32("number", (int)sessionNumber);
+        }
+        if(operation == "times")
+        {
+            sessionNumber *= 2;
+            HttpContext.Session.SetInt32("number", (int)sessionNumber);
+        }
+        if(operation == "random")
+        {
+            Random rand = new Random();
+            sessionNumber += rand.Next(1, 11);
+            HttpContext.Session.SetInt32("number", (int)sessionNumber);
+        }
+        return RedirectToAction("Dashboard");
+    }
+
+    [HttpPost("logout")]
+    public IActionResult Logout()
+    {
+        HttpContext.Session.Clear();
+        return RedirectToAction("Index");
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
