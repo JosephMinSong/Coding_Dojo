d7d49eb421f4dd79af4246926f48461142337de9
Joseph Song <song.josephmin@gmail.com>
2023-07-10 14:05:23 -0700

    initial commit

--- C-Sharp/ASPNET_Core/ORM/FirstConnection/Models/PetModel.cs
+++ C-Sharp/ASPNET_Core/ORM/FirstConnection/Models/PetModel.cs
@@ -1,10 +1,12 @@
 #pragma warning disable CS8618
-
+using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
 namespace FirstConnection.Models;
 
 public class Pet
 {
+    [Key]
+    public int PetId{get;set;}
     public string Name {get;set;}
 
     public string Type {get;set;}
@@ -12,5 +14,9 @@ public class Pet
     public int Age {get;set;}
 
     public bool HasFur {get;set;}
-    
+
+    public DateTime CreatedAt {get;set;} = DateTime.Now;
+
+    public DateTime UpdatedAt {get;set;} = DateTime.Now;
+
 }
