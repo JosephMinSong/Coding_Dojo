9f3d5c1aaad99c68553bb0682015eeda15c3ac34
Joseph Song <song.josephmin@gmail.com>
2023-07-10 23:30:56 -0700

    added show and delete

--- C-Sharp/ASPNET_Core/ORM/CRUDelicious/Controllers/DishController.cs
+++ C-Sharp/ASPNET_Core/ORM/CRUDelicious/Controllers/DishController.cs
@@ -8,15 +8,20 @@ public class DishController : Controller
 {
     private readonly ILogger<DishController> _logger;
 
-    public DishController(ILogger<DishController> logger)
+    private MyContext _context;
+
+    public DishController(ILogger<DishController> logger, MyContext context)
     {
         _logger = logger;
+
+        _context = context;
     }
 
     [HttpGet("")]
     public IActionResult Index()
     {
-        return View();
+        List<Dish> ViewModel = _context.Dishes.ToList();
+        return View(ViewModel);
     }
 
     [HttpGet("/add")]
@@ -25,11 +30,42 @@ public class DishController : Controller
         return View();
     }
 
+    [HttpGet("dishes/{id}")]
+    public IActionResult ShowDish(int id)
+    {
+        Dish? ViewModel = _context.Dishes.FirstOrDefault(x => x.DishId == id);
+        return View(ViewModel);
+    }
+
+    [HttpGet("dishes/{DishId}/edit")]
+    public IActionResult EditDish(int DishId)
+    {
+        Dish? ViewModel = _context.Dishes.FirstOrDefault(x => x.DishId == DishId);
+        return View(ViewModel);
+    }
+
     [HttpPost("ProcessAdd")]
     public IActionResult ProcessAdd(Dish newDish)
     {
-        
-        return View("Index");
+        if(ModelState.IsValid)
+        {
+            _context.Add(newDish);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        else
+        {
+            return View("Add");
+        }
+    }
+
+    [HttpPost("dishes/{DishId}/delete")]
+    public IActionResult Destroy(int DishId)
+    {
+        Dish? DishToDelete = _context.Dishes.SingleOrDefault(x => x.DishId == DishId);
+        _context.Dishes.Remove(DishToDelete);
+        _context.SaveChanges();
+        return RedirectToAction("Index");
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
