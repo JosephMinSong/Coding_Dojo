81e0ce0b9cdb10382d945b9c091e4b60f03d65ef
Joseph Song <song.josephmin@gmail.com>
2023-07-11 19:26:01 -0700

    initial commit

--- C-Sharp/ASPNET_Core/ORM/ChefsNDishes/Models/ChefModel.cs
+++ C-Sharp/ASPNET_Core/ORM/ChefsNDishes/Models/ChefModel.cs
@@ -3,3 +3,38 @@ using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace ChefsNDishes.Models;
+
+public class Chef
+{
+    [Key]
+    public int ChefId {get;set;}
+
+    [Required]
+    public string FirstName {get;set;}
+
+    [Required]
+    public string LastName {get;set;}
+
+    [Required]
+    [DataType(DataType.Date)]
+    [BeforeDate]
+    public DateTime Birthday {get;set;}
+
+    public List<Dish> AllDishes {get;set;} = new List<Dish>();
+}
+
+public class BeforeDateAttribute : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        
+        if((DateTime)value > DateTime.Now)
+        {
+            return new ValidationResult("This is in the future! Please enter a date from the past...");  
+        } 
+        else 
+        {
+            return ValidationResult.Success;
+        }
+    }
+}
--- C-Sharp/ASPNET_Core/ORM/ChefsNDishes/Models/DishModel.cs
+++ C-Sharp/ASPNET_Core/ORM/ChefsNDishes/Models/DishModel.cs
@@ -3,3 +3,19 @@ using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace ChefsNDishes.Models;
+
+public class Dish
+{
+    [Key]
+    public int DishId {get;set;}
+
+    public string Name {get;set;}
+
+    public int Calories {get;set;}
+
+    public int Tastiness {get;set;}
+
+    public int ChefId {get;set;}
+
+    public Dish? Creator {get;set;}
+}
